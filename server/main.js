494664963b6253aa1d22cb1b15e9c2850e885014
alpha <sahashreyam123@gmail.com>
2020-11-23 20:00:48 +0530

    First Commit by Shreyam Saha

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+
+import { Accounts } from 'meteor/accounts-base';
+
+import {TaskCollection} from '../imports/db/TaskCollection';
+
+import '../imports/api/taskMethods';
+
+import '../imports/api/taskPublications';
---
+++ tests/main.js
@@ -0,0 +1,1 @@
+import '../imports/api/taskMethods.tests.js';
